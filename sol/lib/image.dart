a183bbcc75bef62f9059bac37edad2265914413c
Park hyunbin <choiemh@naver.com>
2022-02-04 19:34:53 +0900

    flutter_practice

--- sol/lib/image.dart
+++ sol/lib/image.dart
@@ -1,23 +1,54 @@
 import 'package:flutter/material.dart';
-void main() {
-  runApp(const ImageWidgetApp());
+
+class MapImage extends StatefulWidget {
+  const MapImage({Key? key}) : super(key: key);
+  _MapImageState createState() => _MapImageState();
 }
 
-class ImageWidgetApp extends StatefulWidget {
-  const ImageWidgetApp({Key? key}) : super(key: key);
+class _MapImageState extends State<MapImage> with SingleTickerProviderStateMixin {
+  late Animation _animation;
+  late AnimationController _animationController;
 
   @override
-  Widget build(BuildContext) {
+  void initState() {
+    super.initState();
+    _animationController =
+    new AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    _animation = Tween<double>(begin: 0, end: 10).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeInOut))
+      ..addListener(() {
+        setState(() {
+
+        });
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body : Container(
-        child : Center(
-          child : Column(
-            children : <Widget>[
-              Image.asset('images/map.jpg')
-            ]
-          )
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  GestureDetector(
+                      onTap: () {
+                        if (_animationController.isCompleted) {
+                          _animationController.reverse();
+                        } else {
+                          _animationController.forward();
+                        }
+                      },
+                      child: Container(
+                          child: Transform(
+                            alignment: Alignment.center,
+                            transform: Matrix4.identity(),
+                            child: Image.asset('images/map.jpg'),
+                          )
+                      )
+                  )
+                ]
+            )
         )
-      )
     );
   }
-  }
+}
