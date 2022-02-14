2b06ca2706cc5474d005a0d564b0226ce30f514c
Park hyunbin <choiemh@naver.com>
2022-02-15 01:23:20 +0900

    listview 추가

--- mytree/lib/main.dart
+++ mytree/lib/main.dart
@@ -6,8 +6,6 @@ import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 
-
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -15,9 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Google Maps Demo',
-        debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar : AppBar(
+        appBar: AppBar(
           title: Text('My tree'),
         ),
         body: Stack(
@@ -31,18 +29,17 @@ class MyApp extends StatelessWidget {
               ),
             ),
             Padding(
-              padding : const EdgeInsets.all(8.0),
-              child : Container(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
                 child: MapSample(),
-                ),
               ),
-
+            ),
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Align(
                 alignment: Alignment.topRight,
                 child: FloatingActionButton(
-                  onPressed: () => print('button pressed'),
+                  onPressed: _treelistview,
                   materialTapTargetSize: MaterialTapTargetSize.padded,
                   backgroundColor: Colors.green,
                   child: const Icon(Icons.search, size: 36.0),
@@ -52,9 +49,18 @@ class MyApp extends StatelessWidget {
           ],
         ),
       ),
-
     );
   }
+}
+
+void _treelistview() {
+  ListView.builder(itemBuilder: (context, index) {
+    return ListTile(
+      leading: Image.asset('image/100%.png'),
+      title: Text('Seoul, Korea'),
+      subtitle: Text('#1'),
+    );
+  });
 }
 
 class MapSample extends StatefulWidget {
@@ -66,34 +72,38 @@ class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.5642135, 126.58),
-    zoom: 14.4746,
+    target: LatLng(37.566, 126.97),
+    zoom: 11.5,
   );
 
-  List<Marker> _markers = [];
-
-
-  @override
-  void initState() {
-    super.initState();
-    _markers.add(Marker(
-        markerId: MarkerId("1"),
+  Set<Marker> _createMarker() {
+    return {
+      Marker(
+          markerId: MarkerId("marker_1"),
+          position: LatLng(37.5642135, 127.0016985),
+          draggable: true,
+          icon:
+              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+          infoWindow: InfoWindow(
+            title: 'My First Tree',
+            snippet: '220213',
+          )),
+      Marker(
+        markerId: MarkerId("marker_2"),
+        infoWindow: InfoWindow(title: 'My Second Tree', snippet: '220214'),
         draggable: true,
-        icon: BitmapDescriptor.defaultMarker,
-        infoWindow: InfoWindow(
-          title: '0213',
-          snippet: 'Tree is growing well!',
-        ),
-        position: LatLng(37.5642135, 127.0016985)));
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+        position: LatLng(36.33, 127.4),
+      ),
+    };
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: GoogleMap(
         mapType: MapType.normal,
-        markers: Set.from(_markers),
+        markers: Set.from(_createMarker()),
         initialCameraPosition: _kGooglePlex,
         onCameraMove: (_) {},
         myLocationButtonEnabled: false,
